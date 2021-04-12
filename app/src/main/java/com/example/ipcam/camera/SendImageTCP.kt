17b673033a26b8c8ca8dddca27280bdf632406d5
SebastianTillema <strasmussen8@gmail.com>
2021-04-12 17:03:16 +0200

    Before refactoring

--- app/src/main/java/com/example/ipcam/camera/SendImageTCP.kt
+++ app/src/main/java/com/example/ipcam/camera/SendImageTCP.kt
@@ -11,13 +11,11 @@ class SendImageTCP(private val imageQueue: BlockingQueue<ByteArray>) : Runnable 
 
     override fun run() {
         var socket: Socket? = null
-        var dataOutputStream: DataOutputStream? = null
         var outputStream: OutputStream? = null
 
         try {
-            socket = Socket("192.168.1.74", 9782)
+            socket = Socket("192.168.1.74", 9783)
             outputStream = socket.getOutputStream()
-            dataOutputStream = DataOutputStream(outputStream)
 
             var bytes: ByteArray;
             while (!Thread.currentThread().isInterrupted) {
@@ -27,16 +25,12 @@ class SendImageTCP(private val imageQueue: BlockingQueue<ByteArray>) : Runnable 
                 Log.d("THIS_", "data" + bytesToHex(bytes))
                 outputStream.write(intToByteArray(bytes.size))
                 outputStream.write(bytes)
-
-                //dataOutputStream.writeInt(bytes.size)
-                //dataOutputStream.write(bytes)
             }
         } catch (e: IOException) {
             e.printStackTrace()
         }
         finally {
             outputStream?.close()
-            //dataOutputStream?.close()
             socket?.close()
         }
     }
